13e953b50385dc2fb763187a4b316839cdb44d74
Ирина <ira.sheina.92@gmail.com>
2023-11-16 12:47:51 +0300

    добавлено решение Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,106 @@
+﻿// // Задача_003 Автотест
+using System;
+
+public class Answer
+{
+    public static void MultiplyIfPossible(int[,] matrixA, int[,] matrixB)
+    { // Введите свое решение ниже
+    int columnsA = matrixA.GetLength(1);
+    int rowB = matrixB.GetLength(0);
+
+    if (columnsA != rowB)
+    {
+        Console.WriteLine("It is impossible to multiply.");
+    }
+    else
+    {
+        int[,] result = MatrixMultiplication(matrixA, matrixB);
+        PrintMatrix(result);
+    }
+    }
+
+    public static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {  // Введите свое решение ниже
+
+    int m = matrixA.GetLength(0);
+    int n = matrixA.GetLength(1);
+    int p = matrixB.GetLength(1);
+
+    int[,] resultMatrix = new int[m, p];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                resultMatrix[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return resultMatrix;
+    }
+
+    public static void PrintMatrix(int[,] matrix)
+    {  // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+    }
+ // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[,] matrix;
+
+        if (args.Length == 0)
+        {
+            // Если аргументы не переданы, используем матрицу по умолчанию
+            matrix = new int[,]
+            {
+                {5, 2},
+                {8, 1}
+            };
+        }
+        else
+        {
+            // Иначе, парсим аргументы в двумерный массив
+            string[] rows = args[0].Split(';');
+            matrix = new int[rows.Length, rows[0].Split(',').Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string[] elements = rows[i].Split(',');
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (int.TryParse(elements[j], out int number))
+                    {
+                        matrix[i, j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка при парсинге аргумента {elements[j]}.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        int[,] matrixB = {
+            {5, 6},
+            {7, 8}
+        };
+
+        Console.WriteLine("\nMatrix B:");
+        PrintMatrix(matrixB);
+
+        Console.WriteLine("\nMultiplication result:");
+
+        MultiplyIfPossible(matrix, matrixB);
+    }
+}
